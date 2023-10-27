68370a4aa9c00724a0a26f5c98052112eebd7129
roflganker <abominati0n@protonmail.com>
2023-10-27 12:09:47 +0000

    Prepared deployment configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+function getenv(key: string): string {
+  const env = process.env[key];
+  if (!env) throw new Error(`Missing process.env.${key}`);
+  return env;
+}
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    goerli: {
+      chainId: 5,
+      url: getenv('GOERLI_RPC_URL'),
+      accounts: [getenv('DEPLOYER_PRIV_KEY')],
+    },
+    mainnet: {
+      chainId: 1,
+      url: getenv('MAINNET_RPC_URL'),
+      accounts: [getenv('DEPLOYER_PRIV_KEY')],
+    }
+  },
+  etherscan: {
+    apiKey: getenv('ETHERSCAN_API_KEY'),
+  },
 };
 
 export default config;
