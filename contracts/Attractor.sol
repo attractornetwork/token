5b6e594e61f831475a5316c7fe5e8bf73cbf6779
roflganker <abominati0n@protonmail.com>
2023-10-27 12:09:47 +0000

    Made Attractor smart-contract

---
+++ contracts/Attractor.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol';
+import {ERC20Burnable} from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
+import {ERC20Capped} from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol';
+import {AccessControl} from '@openzeppelin/contracts/access/AccessControl.sol';
+
+contract Attractor is ERC20Capped, ERC20Permit, ERC20Burnable, AccessControl {
+    constructor()
+        ERC20("Attractor", "ATTRA")
+        ERC20Permit("Attractor")
+        ERC20Capped(1e9 * 1e18)
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    function _mint(address account, uint256 amount) internal virtual override(ERC20Capped, ERC20) {
+        ERC20Capped._mint(account, amount);
+    }
+
+    function mint(address account, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(account, amount);
+    }
+}
